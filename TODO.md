0376c8a414cc29ef0cb33883a0ccedfd7d5cf330
Tanveer Raza <tanveerraza789@gmail.com>
2023-11-13 03:31:54 +0530

    feat: reader implementa IntoIter

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO:
+
+1. Add config file
+2. Make FastaReader Iteratable
--- src/reader.rs
+++ src/reader.rs
@@ -1,6 +1,6 @@
 use std::{fmt::Display, fs::File, io::BufReader, path::Path};
 
-use bio::io::fasta::Records;
+use bio::io::fasta::{Record, Records};
 
 #[derive(Debug, thiserror::Error)]
 pub enum FastaReaderError {
@@ -28,5 +28,27 @@ impl FastaReader {
 
     pub fn records(self) -> Records<BufReader<File>> {
         self.inner
+    }
+}
+
+impl IntoIterator for FastaReader {
+    type Item = Record;
+
+    type IntoIter = FastaReaderIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        todo!()
+    }
+}
+
+pub struct FastaReaderIter {
+    inner: Records<BufReader<File>>,
+}
+
+impl Iterator for FastaReaderIter {
+    type Item = Record;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next().map(|rec| rec.ok()).flatten()
     }
 }
