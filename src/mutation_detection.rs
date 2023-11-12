2a1faae034a45e5b099dddeaab1b93df2e6c5445
Tanveer Raza <tanveerraza789@gmail.com>
2023-11-12 22:22:29 +0530

    fix: remove pairwise form mutation
    Pairwise moved to DIffStat

--- src/mutation_detection.rs
+++ src/mutation_detection.rs
@@ -16,7 +16,6 @@ where
     F: MatchFunc + Clone + Display,
 {
     diffstat: &'m DiffStat<'m, F>,
-    pairwise_alignment: Option<Alignment>,
 }
 
 impl<'m, F> Muatation<'m, F>
@@ -30,7 +29,6 @@ where
     {
         Self {
             diffstat: diffstat.as_ref(),
-            pairwise_alignment: None,
         }
     }
 }
